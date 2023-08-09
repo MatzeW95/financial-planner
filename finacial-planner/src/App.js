f5f083979c3009621088dfabc26545ba50908422
Matthias Wagner <22793681+MatzeW95@users.noreply.github.com>
2023-08-09 19:32:16 +0200

    added pages

--- finacial-planner/src/App.js
+++ finacial-planner/src/App.js
@@ -1,9 +1,19 @@
 import './style.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Dashboard from "./pages/Dashboard";
+
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
