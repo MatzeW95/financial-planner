90206ab7aa9507704b6f7bbc8447d10f802f6f35
Matthias Wagner <22793681+MatzeW95@users.noreply.github.com>
2023-08-17 16:38:36 +0200

    Added savings based on expenses

--- finacial-planner/src/components/ChartSavingsAll.js
+++ finacial-planner/src/components/ChartSavingsAll.js
@@ -1,5 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import { Pie } from 'react-chartjs-2';
+import jsonData from '../data/data.json';
+
+const generateColors = count => {
+  const colors = [];
+
+  const maxLightness = 50;
+  const minLightness = 20;
+  const lightnessDifference = maxLightness - minLightness;
+
+  for (let i = 0; i < count; i++) {
+    const hue = 208; // Hue for the base color (#0085f2)
+    const saturation = 100; // Max saturation
+    const lightness = maxLightness - (i * (lightnessDifference / count)); // Adjust lightness
+    colors.push(`hsl(${hue}, ${saturation}%, ${lightness}%)`);
+  }
+  return colors;
+};
+
 const ChartSavingsAll = () => {
-    return <h1>ChartSavingsAll</h1>;
+  const [savingsData, setsavingsData] = useState([]);
+
+  useEffect(() => {
+    setsavingsData(jsonData.data.savings);
+  }, []);
+
+  const data = {
+    labels: savingsData.map(savings => savings.name),
+    datasets: [
+      {
+        data: savingsData.map(savings => parseFloat(savings.amount)),
+        backgroundColor: generateColors(savingsData.length) // Generate dynamic colors
+      }
+    ]
   };
-  
-  export default ChartSavingsAll;
+
+  return (
+    <div>
+      <Pie data={data} />
+    </div>
+  );
+};
+
+export default ChartSavingsAll;
