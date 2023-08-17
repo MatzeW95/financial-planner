d9ab952a06f47612e13ddf4452f97f905b7fd847
Matthias Wagner <22793681+MatzeW95@users.noreply.github.com>
2023-08-17 16:38:53 +0200

    Added incomes based on expenses

--- finacial-planner/src/components/ChartIncomesAll.js
+++ finacial-planner/src/components/ChartIncomesAll.js
@@ -1,5 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import { Pie } from 'react-chartjs-2';
+import jsonData from '../data/data.json';
+
+const generateColors = count => {
+  const colors = [];
+
+  const maxLightness = 50;
+  const minLightness = 20;
+  const lightnessDifference = maxLightness - minLightness;
+
+  for (let i = 0; i < count; i++) {
+    const hue = 208; // Hue for the base color (#0085f2)
+    const saturation = 100; // Max saturation
+    const lightness = maxLightness - (i * (lightnessDifference / count)); // Adjust lightness
+    colors.push(`hsl(${hue}, ${saturation}%, ${lightness}%)`);
+  }
+  return colors;
+};
+
 const ChartIncomesAll = () => {
-    return <h1>ChartIncomesAll</h1>;
+  const [incomesData, setincomesData] = useState([]);
+
+  useEffect(() => {
+    setincomesData(jsonData.data.incomes);
+  }, []);
+
+  const data = {
+    labels: incomesData.map(incomes => incomes.name),
+    datasets: [
+      {
+        data: incomesData.map(incomes => parseFloat(incomes.amount)),
+        backgroundColor: generateColors(incomesData.length) // Generate dynamic colors
+      }
+    ]
   };
-  
-  export default ChartIncomesAll;
+
+  return (
+    <div>
+      <Pie data={data} />
+    </div>
+  );
+};
+
+export default ChartIncomesAll;
