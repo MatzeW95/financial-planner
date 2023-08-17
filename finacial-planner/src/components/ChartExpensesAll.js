bddf018eadeecffff18ace4786a2f1274bf73a5b
Matthias Wagner <22793681+MatzeW95@users.noreply.github.com>
2023-08-17 16:17:15 +0200

    added first iteration pie ChartExpensesAll

--- finacial-planner/src/components/ChartExpensesAll.js
+++ finacial-planner/src/components/ChartExpensesAll.js
@@ -1,5 +1,41 @@
-const ChartExpensesAll = () => {
-    return <h1>ChartExpensesAll</h1>;
-  };
+import React, { useState, useEffect } from 'react';
+import { Pie } from 'react-chartjs-2';
+import jsonData from '../data/data.json';
+
+const generateColors = count => {
+  const colors = [];
   
-  export default ChartExpensesAll;
+  for (let i = 0; i < count; i++) {
+    const hue = 208; // Hue for the base color (#0085f2)
+    const saturation = 100; // Max saturation
+    const lightness = 60 - (i * 5); // Decrease lightness for each color
+    colors.push(`hsl(${hue}, ${saturation}%, ${lightness}%)`);
+  }
+  return colors;
+};
+
+const ChartExpensesAll = () => {
+  const [expenseData, setExpenseData] = useState([]);
+
+  useEffect(() => {
+    setExpenseData(jsonData.data.expenses);
+  }, []);
+
+  const data = {
+    labels: expenseData.map(expense => expense.name),
+    datasets: [
+      {
+        data: expenseData.map(expense => parseFloat(expense.amount)),
+        backgroundColor: generateColors(expenseData.length) // Generate dynamic colors
+      }
+    ]
+  };
+
+  return (
+    <div>
+      <Pie data={data} />
+    </div>
+  );
+};
+
+export default ChartExpensesAll;
